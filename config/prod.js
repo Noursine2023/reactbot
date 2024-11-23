9c5364d762f246683be444dd579789a090c2a281
Noursine <noursine.baldi@gmail.com>
2024-11-23 18:59:11 +0100

    Remove private key from version control

--- config/prod.js
+++
@@ -1,9 +0,0 @@
-const { googleClientEmail, googlePrivateKey } = require("./dev");
-
-module.exports = {
-    googleProjectID:process.env.GOOGLE_PROJECT_ID,
-    dialogFlowSessionID:'procee.env.DIALOGFLOW_SESSION_ID',
-    dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
-    googleClientEmail:process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey:process.env.GOOGLE_PRIVATE_KEY
-}
