dfc6c7bb442f7bc17c000ff5a7ad609d40c20ad6
Noursine <noursine.baldi@gmail.com>
2024-11-23 21:03:05 +0100

    Updated chatbot logic and configurations

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app);
