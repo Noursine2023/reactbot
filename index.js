4837bd7ec5f6741fe16fac7ce2604794a1ddaf44
Noursine <noursine.baldi@gmail.com>
2024-11-23 22:13:03 +0100

    Updated chatbot logic and configurations

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 
+// Import routes
 require('./routes/dialogFlowRoutes')(app);
 
-const PORT = process.env.PORT||5000;
-app.listen(PORT, () => {
+// Start the server, binding to the correct port (use process.env.PORT if available)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
